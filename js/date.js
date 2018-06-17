aa51e620f038eefff693da8324a61cf6b18245dc
calvinweb <38599774+calvinweb@users.noreply.github.com>
2018-06-17 18:33:54 +0800

    Update date.js

--- js/date.js
+++ js/date.js
@@ -5,7 +5,7 @@ function startTime(){
 	var s=today.getSeconds();// 在小于10的数字前加一个‘0’a
 	m=checkTime(m);
 	s=checkTime(s);
-	document.getElementById('t').innerHTML= h+":"+m+":"+s+week();
+	document.getElementById('t').innerHTML= d24(h)+":"+m+":"+s+week();
 	t=setTimeout(function(){startTime()},500);
 }
 function checkTime(i){
