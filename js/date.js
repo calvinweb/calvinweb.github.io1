663465f90e1bdd94a88cce5e3e84deaed489a5d1
calvinweb <38599774+calvinweb@users.noreply.github.com>
2018-06-17 16:25:23 +0800

    Update date.js

--- js/date.js
+++ js/date.js
@@ -2,7 +2,7 @@ function startTime(){
 	var today=new Date();
 	var h=today.getHours();
 	var m=today.getMinutes();
-	var s=today.getSeconds();// 在小于10的数字前加一个‘0’
+	var s=today.getSeconds();// 在小于10的数字前加一个‘0’a
 	m=checkTime(m);
 	s=checkTime(s);
 	document.getElementById('t').innerHTML= h+":"+m+":"+s+week();
